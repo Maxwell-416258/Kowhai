f2f206fb37018f0f8cf1145ef533382cd9f9b461
xuzixuan <xzx527416@gmail.com>
2024-11-23 17:43:07 +0800

    fix:关闭cors同源策略

--- apps/router.go
+++ apps/router.go
@@ -1,15 +1,26 @@
 package apps
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
+	"time"
 	"vidspark/apps/user"
 	_ "vidspark/docs"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.New()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
