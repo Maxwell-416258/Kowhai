e9cf7dc06c18ad024b19884b9d77d2d52bbf09e3
xuzixuan <xzx527416@gmail.com>
2025-01-15 16:32:24 +0800

    fix:跨域问题

--- apps/router.go
+++ apps/router.go
@@ -11,13 +11,14 @@ import (
 func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger()) //启用logger中间件
+
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins:     []string{"http://http://119.45.154.194:9001", "http://localhost:3000"}, // 设置允许的源
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},                    // 设置允许的请求方法
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},                    // 设置允许的请求头
+		ExposeHeaders:    []string{"Content-Length", "Content-Type"},                             // 设置可以暴露的响应头
+		AllowCredentials: true,                                                                   // 是否允许凭据
+		MaxAge:           12 * time.Hour,                                                         // 缓存预检请求的结果
 	}))
 
 	v1 := r.Group("/v1")
